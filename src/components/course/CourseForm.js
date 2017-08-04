a979792d61b1c6925033a44b8eeee79f7f18ced0
Jose Alanya <jose.alanya@gmail.com>
2017-08-04 17:56:40 -0500

    FEAT: Async Writes in Redux - Create Form Input Components

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -7,39 +7,39 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => 
     <form>
       <h1>Manage Course</h1>
       <TextInput
-        name='title'
-        label='Title'
+        name="title"
+        label="Title"
         value={course.title}
         onChange={onChange}
-        error={error.title} />
+        error={errors.title} />
 
       <SelectInput
-        name='authorId'
-        label='Author'
+        name="authorId"
+        label="Author"
         value={course.authorId}
         defaultOption="Select Author"
         options={allAuthors}
         onChange={onChange} error={errors.authorId} />
 
       <TextInput
-        name='category'
-        label='Category'
+        name="category"
+        label="Category"
         value={course.category}
         onChange={onChange}
         error={errors.category} />
 
       <TextInput
-        name='length'
-        label='Length'
+        name="length"
+        label="Length"
         value={course.length}
         onChange={onChange}
-        error={error.length} />
+        error={errors.length} />
 
       <input
-        type='submit'
+        type="submit"
         disabled={loading}
-        value={loading ? 'Saving...':  'Save'}
-        className='btn btn-primary'
+        value={loading ? "Saving...":  "Save"}
+        className="btn btn-primary"
         onClick={onSave} />
     </form>
   );
