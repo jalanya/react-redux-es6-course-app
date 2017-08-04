b1bcb800f2f6fb6c71156f6018a9abab115ed9fb
Jose Alanya <jose.alanya@gmail.com>
2017-08-04 18:34:48 -0500

    FEAT: Async Writes in Redux - Use Manage Course Form

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -1,13 +1,13 @@
 import React, {PropTypes} from 'react';
 
 const TextInput = ({name, label, onChange, placeHolder, value, error}) => {
-  let wrapperClass = 'form-group';
+  let wrapperClass = "form-group";
   if (error && error.length > 0) {
     wrapperClass += " " + "has-error";
   }
 
   return (
-    <div className="wrapperClass">
+    <div className={wrapperClass}>
       <label htmlFor={name}>{label}</label>
       <div className="field">
         <input
